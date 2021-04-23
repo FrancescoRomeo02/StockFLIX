0158c16d79d2434eda23369b39acb80cdd09ca15
FrancescoRomeo02 <romeo.francesco.2002@gmail.com>
2021-04-23 22:52:09 +0200

    api semi finite

--- App/python/pydata_apiv2.py
+++ App/python/pydata_apiv2.py
@@ -6,7 +6,6 @@ api_key = "D99JTY63MSIF56G9"
 
 
 def get_ts(symbol, data_type):
-
     ts = TimeSeries(key=api_key, output_format='pandas')
     if (data_type == 'y'):
         data, meta_data = ts.get_daily_adjusted(symbol=symbol,
@@ -38,13 +37,9 @@ def get_ts(symbol, data_type):
     nice_data.to_csv(fname)
 
 
-symbols = ['AAPL', 'GOOG', 'TSLA', 'MSFT']
-
+symbols = [a, p, s]  #get name from php
+data_input = [d, y]  #get type from php
+i = 0
 for s in symbols:
-    data_input = input(f'{s} daily o year: ')
-    get_ts(s, data_input)
-
-# 800 597 3978      codice per pratica 24/48h poste.
-# calcolo media mobile
-# sessione al login
-# Numero sul talloncino della garanzia.
+    for data in data_input:
+        get_ts(s, data)
