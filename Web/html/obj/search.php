f2c551e76e5272a46f51c2f653a4b0354923bcf9
FrancescoRomeo02 <romeo.francesco.2002@gmail.com>
2021-04-25 00:34:01 +0200

    search bar

--- Web/html/obj/search.php
+++ Web/html/obj/search.php
@@ -1,42 +1,38 @@
-<!DOCTYPE html>
+!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Autocomplete Script</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <link rel="stylesheet" href="style.css">
+    <script>
+        $(function() {
+            $("#country").keyup(function() {
+                var countryName = $(this).val();
+                $.ajax({
+                        method: "POST",
+                        url: "getCountry.php",
+                        data: {
+                            country: countryName
+                        }
+                    })
+                    .done(function(data) {
+                        $("#suggestions").show();
+                        $("#suggestions").html(data);
+                    });
+            });
+        });
+    </script>
 </head>
 
 <body>
-    <div class="frmSearch">
-        <input type="text" id="search-box" placeholder="Country Name" />
-        <div id="suggesstion-box"></div>
+
+    <div class="countrySearch">
+        <input type="text" id="country" placeholder="Country">
+        <div id="suggestions"></div>
     </div>
+
 </body>
-<script>
-    $(document).ready(function() {
-        $("#search-box").keyup(function() {
-            $.ajax({
-                type: "POST",
-                url: "readCountry.php",
-                data: 'keyword=' + $(this).val(),
-                beforeSend: function() {
-                    $("#search-box").css("background", "#FFF url(LoaderIcon.gif) no-repeat 165px");
-                },
-                success: function(data) {
-                    $("#suggesstion-box").show();
-                    $("#suggesstion-box").html(data);
-                    $("#search-box").css("background", "#FFF");
-                }
-            });
-        });
-    });
-    //To select country name
-    function selectCountry(val) {
-        $("#search-box").val(val);
-        $("#suggesstion-box").hide();
-    }
-</script>
 
 </html>
