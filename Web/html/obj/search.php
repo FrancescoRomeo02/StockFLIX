5f8a0d01ee867610e3ce30e81004802ee4f8bee5
FrancescoRomeo02 <romeo.francesco.2002@gmail.com>
2021-04-25 01:11:45 +0200

    search bar

--- Web/html/obj/search.php
+++ Web/html/obj/search.php
@@ -3,6 +3,7 @@
 
 <head>
     <meta charset="utf-8">
+    <title>jQuery UI autocomplete</title>
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
@@ -15,24 +16,52 @@
                     this.widget().menu("option", "items", "> :not(.ui-autocomplete-category)");
                 },
                 _renderMenu: function(ul, items) {
-                    var that = this;
+                    var that = this,
+                        currentCategory = "";
                     $.each(items, function(index, item) {
                         var li;
+                        if (item.category != currentCategory) {
+                            ul.append("<li class='ui-autocomplete-category'>" + item.category + "</li>");
+                            currentCategory = item.category;
+                        }
                         li = that._renderItemData(ul, item);
                         if (item.category) {
-                            li.attr("aria-label", item.label);
+                            li.attr("aria-label", item.category + " : " + item.label);
                         }
                     });
                 }
             });
-
-            $importer = new CsvImporter("../../../Data/stock.csv", true);
-            while ($data = $importer) {
-                print_r($data);
-            }
-
+            var italianTeams = [{
+                    label: "Juventus",
+                    category: "North"
+                },
+                {
+                    label: "Inter",
+                    category: "North"
+                },
+                {
+                    label: "Milan",
+                    category: "North"
+                },
+                {
+                    label: "Roma",
+                    category: "Center"
+                },
+                {
+                    label: "Lazio",
+                    category: "Center"
+                },
+                {
+                    label: "Napoli",
+                    category: "South"
+                },
+                {
+                    label: "Palermo",
+                    category: "South"
+                }
+            ];
             $("#tags").autocompleteCategory({
-                source: stocks
+                source: italianTeams
             });
         });
     </script>
@@ -40,7 +69,7 @@
 
 <body>
     <div class="ui-widget">
-        <label for="tags"></label>
+        <label for="tags">Squadre: </label>
         <input id="tags">
     </div>
 </body>
