3c749790838de26e6881bb20b051a8ab3d2a8247
FrancescoRomeo02 <romeo.francesco.2002@gmail.com>
2021-04-26 21:44:52 +0200

    temp data

--- temp/ ajax-db-search.php
+++ temp/ ajax-db-search.php
@@ -1,14 +1,14 @@
 <?php
-require_once "connection.php";
+require_once "db.php";
 
 if (isset($_POST['query'])) {
 
-    $query = "SELECT * FROM stock_name WHERE name LIKE '{$_POST['query']}%' LIMIT 7";
+    $query = "SELECT * FROM stock_name WHERE Symbol LIKE '{$_POST['query']}%' LIMIT 7";
     $result = mysqli_query($conn, $query);
 
     if (mysqli_num_rows($result) > 0) {
         while ($user = mysqli_fetch_array($result)) {
-            echo $user['name'] . '     ' . $user['COL 2'] . "<br/>";
+            echo $user['Symbol'] . '     ' . $user['Security Name'] . "<br/>";
         }
     } else {
         echo "<p style='color:red'>User not found...</p>";
