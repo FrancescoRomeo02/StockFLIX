f15dde066d8e9b5afc1bf5251d04651d0d762678
FrancescoRomeo02 <romeo.francesco.2002@gmail.com>
2021-04-27 11:15:05 +0200

    search v1

--- temp/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-$servername = 'localhost';
-$username = 'romeofrancesco';
-$password = '';
-$dbname = "my_romeofrancesco";
-$conn = mysqli_connect($servername, $username, $password, "$dbname");
-if (!$conn) {
-    die('Could not Connect MySql Server');
-}
