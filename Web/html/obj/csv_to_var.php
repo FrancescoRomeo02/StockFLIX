e0905020dc0c0cc63b1041c9518273129af684aa
FrancescoRomeo02 <romeo.francesco.2002@gmail.com>
2021-04-27 17:28:53 +0200

    search v1

---
+++ Web/html/obj/csv_to_var.php
@@ -0,0 +1,41 @@
+<html>
+
+<head>
+    <title>CSV File to HTML Table Using AJAX jQuery</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+
+</body>
+
+</html>
+
+<script>
+    function data_csv() {
+        var obj_stack = [];
+        $.ajax({
+            url: "stock.csv",
+            dataType: "text",
+            success: function(data) {
+                var stock_data = data.split(/\r?\n|\r/);
+                var symbol = [];
+                var sec_name = [];
+                stock_data.forEach(element => {
+                    data = element.split(',');
+                    obj_stack.push({
+                        'symbol': data[0],
+                        'name': data[1]
+                    });
+                });
+                //search
+
+                //search
+            },
+        });
+    };
+
+    var temp = data_csv()
+</script>
