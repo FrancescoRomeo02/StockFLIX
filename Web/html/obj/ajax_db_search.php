3ce884bb71528ef3b5e0288ef1149f8fd7d9472e
FrancescoRomeo02 <romeo.francesco.2002@gmail.com>
2021-04-27 22:23:10 +0200

    search v3

--- Web/html/obj/ajax_db_search.php
+++ Web/html/obj/ajax_db_search.php
@@ -5,12 +5,18 @@ if (isset($_POST['query'])) {
 
     $query = "SELECT * FROM stock_name WHERE Symbol LIKE '{$_POST['query']}%' LIMIT 7";
     $result = mysqli_query($con, $query);
-
+    $count = 1;
     if (mysqli_num_rows($result) > 0) {
         while ($stock = mysqli_fetch_array($result)) {
-            echo $stock['Symbol'] . '     ' . $stock['Security Name'] . "<br/>";
+            $count += 1;
+            echo "
+            <li class='item'>
+                <p class='symbol" . "p" . $count . ">" . $stock['Symbol'] . "</p>
+                <p class='sec_name'>" . $stock['Security Name'] . "</p>
+            </li>
+            ";
         }
     } else {
-        echo "<p style='color:red'>stock not found...</p>";
+        echo "<li class='item'><p class='not_in'>Azione inesistente</p></li>";
     }
 }
