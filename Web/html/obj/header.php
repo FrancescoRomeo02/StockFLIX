30d442b719f198b84fc9ccb9bb225c1b2c281b61
FrancescoRomeo02 <romeo.francesco.2002@gmail.com>
2021-05-07 12:38:08 +0200

    session var

--- Web/html/obj/header.php
+++ Web/html/obj/header.php
@@ -6,13 +6,13 @@ $_SESSION['db_name'] =   'my_romeofrancesco';
 $con = mysqli_connect('localhost', $_SESSION['name_user'], '');
 $con_db = (mysqli_select_db($con, $_SESSION['db_name']));
 
-$query_data = "SELECT hash FROM utenti_free WHERE email = '$email' AND password = '$password'";
-error_log($query_data);
+$query_data = "SELECT hash FROM utenti_free WHERE email = '$_SESSION[email]' AND password = '$_SESSION[code]'";
+echo $query_data;
 $temp = mysqli_query($con, $query_data);
 $data = mysqli_fetch_array($temp);
-
+print_r($data);
 if (isset($_SESSION['login']) && $_SESSION['login'] == $data['hash']) {
-    $_SESSION['state'] = true;
+    $_SESSION['state'] = 1;
 } else {
-    $_SESSION['state'] = false;
+    $_SESSION['state'] = 0;
 }
