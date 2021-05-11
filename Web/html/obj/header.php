224ef2c032d523b501fbf3516e2a2cba1ff49799
FrancescoRomeo02 <romeo.francesco.2002@gmail.com>
2021-05-11 09:40:03 +0200

    cookie control

--- Web/html/obj/header.php
+++ Web/html/obj/header.php
@@ -7,10 +7,8 @@ $con = mysqli_connect('localhost', $_SESSION['name_user'], '');
 $con_db = (mysqli_select_db($con, $_SESSION['db_name']));
 
 $query_data = "SELECT hash FROM utenti_free WHERE email = '$_SESSION[email]' AND password = '$_SESSION[code]'";
-echo $query_data;
 $temp = mysqli_query($con, $query_data);
-$data = mysqli_fetch_array($temp);
-print_r($data);
+$data = mysqli_fetch_array($temp);;
 if (isset($_SESSION['login']) && $_SESSION['login'] == $data['hash']) {
     $_SESSION['state'] = 1;
 } else {
